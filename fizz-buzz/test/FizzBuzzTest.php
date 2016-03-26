69c5bb4c4da29910acb28db98d1e396ab5ba0c74
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-03-26 23:30:41 +0000

    Refactor: collapse tests with same behavior

--- fizz-buzz/test/FizzBuzzTest.php
+++ fizz-buzz/test/FizzBuzzTest.php
@@ -6,15 +6,11 @@ use FizzBuzz\FizzBuzzGenerator;
 
 class FizzBuzzTest extends \PHPUnit_Framework_TestCase
 {
-    /** @test */
-    public function should_return_1_parsed_to_string()
-    {
-        $this->assertEquals("1", FizzBuzzGenerator::generateFizzBuzzSequenceUpTo(1));
-    }
 
     /** @test */
     public function should_return_the_sequence_up_to_the_given_number_parsed_to_string()
     {
+        $this->assertEquals("1", FizzBuzzGenerator::generateFizzBuzzSequenceUpTo(1));
         $this->assertEquals("1\n2", FizzBuzzGenerator::generateFizzBuzzSequenceUpTo(2));
     }
 }
