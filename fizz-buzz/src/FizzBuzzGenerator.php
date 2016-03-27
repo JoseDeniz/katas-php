ca0a5cbdb0c7ca5e60bd1008071efd274bf6b526
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-03-27 00:02:46 +0000

    Refactor: extract method

--- fizz-buzz/src/FizzBuzzGenerator.php
+++ fizz-buzz/src/FizzBuzzGenerator.php
@@ -12,17 +12,26 @@ class FizzBuzzGenerator
     public static function generateFizzBuzzSequenceUpTo($limit)
     {
         $sequence = "";
-        for ($i = 1; $i <= $limit; $i++) {
-            $sequence = $sequence.self::parseNumber($i);
-        }
+        for ($i = 1; $i <= $limit; $i++)
+            $sequence = $sequence.self::parseNumber($i)."\n";
         return trim($sequence);
     }
 
     private static function parseNumber($number)
     {
-        if ($number % 3 == 0 && $number % 5 == 0) return "\nFizzBuzz";
-        else if ($number % 5 == 0) return "\nBuzz";
-        else if ($number % 3 == 0) return "\nFizz";
-        return "\n".$number;
+        $parsedNumber = self::parseIsMultipleOfThree($number).self::parseIsMultipleOfFive($number);
+        return $parsedNumber === "" ? $number : $parsedNumber;
+    }
+
+    private static function parseIsMultipleOfThree($number)
+    {
+        if ($number % 3 == 0) return "Fizz";
+        return "";
+    }
+
+    private static function parseIsMultipleOfFive($number)
+    {
+        if ($number % 5 == 0) return "Buzz";
+        return "";
     }
 }
