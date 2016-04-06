5b903991f35839ae5057df48c88b99a66576a5b5
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-06 21:56:44 +0100

    Refactor: extract method

--- fizz-buzz/src/FizzBuzz.php
+++ fizz-buzz/src/FizzBuzz.php
@@ -5,34 +5,20 @@ namespace FizzBuzz;
 class FizzBuzz
 {
     const _EMPTY = "";
-
     const FROM_ONE = 1;
     const TO_ONE_HUNDRED = 100;
 
     public function generateFizzBuzzSequence()
     {
-        $sequence = range(self::FROM_ONE, self::TO_ONE_HUNDRED);
-        return array_map(function($number) {
-            return $this->parseNumber($number);
-        }, $sequence);
+        return array_map($this->parseNumber(), range(self::FROM_ONE, self::TO_ONE_HUNDRED));
     }
 
-    /**
-     * @param int $limit
-     * @return string
-     */
-    public function generateFizzBuzzSequenceUpTo($limit)
+    private function parseNumber()
     {
-        $sequence = self::_EMPTY;
-        for ($i = 1; $i <= $limit; $i++)
-            $sequence .= self::parseNumber($i)."\n";
-        return trim($sequence);
-    }
-
-    private function parseNumber($number)
-    {
-        $parsedNumber = self::parseIsMultipleOfThree($number).self::parseIsMultipleOfFive($number);
-        return $parsedNumber === self::_EMPTY ? $number : $parsedNumber;
+        return function ($number) {
+            $parsedNumber = self::parseIsMultipleOfThree($number).self::parseIsMultipleOfFive($number);
+            return $parsedNumber === self::_EMPTY ? $number : $parsedNumber;
+        };
     }
 
     private function parseIsMultipleOfThree($number)
