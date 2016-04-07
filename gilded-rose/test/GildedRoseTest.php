2a1ed8ab622d4a073fea0ab6778260165ad7890a
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-07 17:51:30 +0100

    green: sulfuras_hand_of_ragnaros_doesnt_vary_sell_date_when_update_quality

--- gilded-rose/test/GildedRoseTest.php
+++ gilded-rose/test/GildedRoseTest.php
@@ -19,4 +19,15 @@ class GildedRoseTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($items[0]->quality, 80);
     }
+
+    /** @test */
+    public function sulfuras_hand_of_ragnaros_doesnt_vary_sell_date_when_update_quality()
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 0, 80)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->update_quality();
+
+        $this->assertEquals($items[0]->sell_in, 0);
+    }
+
 }
