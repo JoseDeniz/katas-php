aeeda044503b75d3f451df8c69275ee065a65927
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-07 18:19:40 +0100

    green: backstage_pass_increase_its_quality_when_sellIn_is_greater_than_ten_by_one

---
+++ gilded-rose/test/BackStagePassItemTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GildedRose\Test;
+
+use GildedRose\GildedRose;
+use GildedRose\Item;
+
+class BackStageItemTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  GildedRose */
+    private $gildedRose;
+    /** @var  Item[] */
+    private $items;
+
+    /** @before */
+    public function backStageItemInitialize()
+    {
+        $this->items = [new Item('Backstage passes to a TAFKAL80ETC concert', 12, 20)];
+        $this->gildedRose = new GildedRose($this->items);
+    }
+
+    /** @test */
+    public function
+    backstage_pass_increase_its_quality_when_sellIn_is_greater_than_ten_by_one()
+    {
+        $this->gildedRose->update_quality();
+        $this->assertEquals(21, $this->items[0]->quality);
+    }
+}
