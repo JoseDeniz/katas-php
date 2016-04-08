eb9afdde9024ff8fd22d5afcdfa6e05d735cb8dd
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-08 11:51:29 +0100

    Refactor: introduce constant variable

--- gilded-rose/test/BaseItemTest.php
+++ gilded-rose/test/BaseItemTest.php
@@ -7,12 +7,13 @@ use GildedRose\Item;
 
 class BaseItemTest extends \PHPUnit_Framework_TestCase
 {
+    const ANY_ITEM = 'AnyItem';
 
     /** @test */
     public function
     base_item_quality_decrease_twice_as_fast_when_sellIn_expires()
     {
-        $item = new Item('AnyItem', 0, 10);
+        $item = new Item(self::ANY_ITEM, 0, 10);
         $this->updateQualityFor($item);
         $this->assertEquals(8, $item->quality);
         $this->updateQualityFor($item);
@@ -22,7 +23,7 @@ class BaseItemTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function base_item_quality_is_never_a_negative_value()
     {
-        $item = new Item('AnyItem', 0, 0);
+        $item = new Item(self::ANY_ITEM, 0, 0);
         $this->updateQualityFor($item);
         $this->assertEquals(0, $item->quality);
     }
