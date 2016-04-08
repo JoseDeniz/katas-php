97c970613903fd67064fd140009f33477d230b7c
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-08 12:08:58 +0100

    Refactor: join if statements

--- gilded-rose/src/GildedRose.php
+++ gilded-rose/src/GildedRose.php
@@ -20,12 +20,10 @@ class GildedRose
             $this->decrementSellInFor($item);
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
+                if (!$this->isSulfuras($item) and $item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
-                            if (!$this->isSulfuras($item)) {
-                                $item->quality = $item->quality - 1;
-                            }
+                            $item->quality = $item->quality - 1;
                         }
                     } else {
                         $item->quality = $item->quality - $item->quality;
