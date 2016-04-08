f536669308c82cacdcc5655fb90198a13c6284a2
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-08 12:08:21 +0100

    Refactor: extract method

--- gilded-rose/src/GildedRose.php
+++ gilded-rose/src/GildedRose.php
@@ -15,25 +15,7 @@ class GildedRose
     function update_quality()
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if (!$this->isSulfuras($item)) {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            $item->quality = $item->quality + 1;
-                        }
-                        if ($item->sell_in < 6) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
-            }
+            $this->foo($item);
 
             $this->decrementSellInFor($item);
 
@@ -50,6 +32,30 @@ class GildedRose
                     }
                 } else {
                     if ($item->quality < 50) {
+                        $item->quality = $item->quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $item
+     */
+    private function foo($item)
+    {
+        if (!$this->isSulfuras($item) and $item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->sell_in < 11) {
+                        $item->quality = $item->quality + 1;
+                    }
+                    if ($item->sell_in < 6) {
                         $item->quality = $item->quality + 1;
                     }
                 }
