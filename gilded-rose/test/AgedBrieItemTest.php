aa9623d0d4a548f8d56db1632b593f44deb86b9d
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-08 11:46:31 +0100

    Refactor: replace setUp with method

--- gilded-rose/test/AgedBrieItemTest.php
+++ gilded-rose/test/AgedBrieItemTest.php
@@ -7,23 +7,13 @@ use GildedRose\Item;
 
 class AgedBrieItemTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var  GildedRose */
-    private $gildedRose;
-    /** @var  Item[] */
-    private $items;
-
-    /** @before */
-    public function agedBrieInitialize()
-    {
-        $this->items = [new Item('Aged Brie', 2, 0)];
-        $this->gildedRose = new GildedRose($this->items);
-    }
-
+    
     /** @test */
     public function aged_brie_increase_its_quality_when_time_passes()
     {
-        $this->gildedRose->update_quality();
-        $this->assertEquals(1, $this->items[0]->quality);
+        $item = new Item('Aged Brie', 10, 0);
+        $this->updateQualityFor($item);
+        $this->assertEquals(1, $item->quality);
     }
 
     /** @test */
