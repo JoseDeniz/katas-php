65ae1a2a9cca4d96fbd51d9020b7b5c63914fbde
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-08 11:38:41 +0100

    green: aged_brie_never_passes_a_quality_of_fifty

--- gilded-rose/test/AgedBrieItemTest.php
+++ gilded-rose/test/AgedBrieItemTest.php
@@ -25,4 +25,18 @@ class AgedBrieItemTest extends \PHPUnit_Framework_TestCase
         $this->gildedRose->update_quality();
         $this->assertEquals(1, $this->items[0]->quality);
     }
+
+    /** @test */
+    public function aged_brie_never_passes_a_quality_of_fifty()
+    {
+        $item = new Item('Aged Brie', 2, 50);
+        $this->updateQualityFor($item);
+        $this->assertEquals(50, $item->quality);
+    }
+
+    private function updateQualityFor($item)
+    {
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->update_quality();
+    }
 }
