b177ff9c6f7eb573ec7eb4633eb79bbf7e5c9f59
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-14 17:46:05 +0100

    Add new test case in base_item_quality_should_never_be_a_negative_value

--- gilded-rose/src/BaseItem.php
+++ gilded-rose/src/BaseItem.php
@@ -16,6 +16,8 @@ class BaseItem extends Item
 
     private function decreaseQuality()
     {
-        $this->quality = $this->quality - 1;
+        if ($this->quality > 0) {
+            $this->quality = $this->quality - 1;
+        }
     }
 }
--- gilded-rose/test/BaseItemTest.php
+++ gilded-rose/test/BaseItemTest.php
@@ -21,11 +21,14 @@ class BaseItemTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    public function base_item_quality_is_never_a_negative_value()
+    public function base_item_quality_should_never_be_a_negative_value()
     {
-        $item = new Item(self::ANY_ITEM, 0, 0);
-        $this->updateQualityFor($item);
-        $this->assertEquals(0, $item->quality);
+        $item_one = new Item(self::ANY_ITEM, 0, 2);
+        $item_two = new Item(self::ANY_ITEM, 0, 1);
+        $this->updateQualityFor($item_one);
+        $this->updateQualityFor($item_two);
+        $this->assertEquals(0, $item_one->quality);
+        $this->assertEquals(0, $item_two->quality);
     }
 
     private function updateQualityFor($item)
